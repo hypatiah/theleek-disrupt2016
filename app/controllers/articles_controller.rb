59e9a6a10a4b1da6cff6d2f9ad4d39fac3798833
Will Friebel <will.friebel@gmail.com>
2016-09-10 19:50:24 -0700

    Get article info from api, send data to Watson

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,25 @@
+require 'alchemyapi'
+
 class ArticlesController < ApplicationController
   def index
     @articles = HTTParty.get('https://public-api.wordpress.com/wp/v2/sites/techcrunch.com/posts')
+    @article = @articles[0]['title']['rendered']
+    alchemyapi = AlchemyAPI.new()
+    myText = @article
+    response = alchemyapi.relations("text", myText)
+    for relation in response['relations']
+      if relation.key?('subject')
+        puts 'subject: ' + relation['subject']['text']
+      end
+
+      if relation.key?('action')
+        puts 'action: ' + relation['action']['text']
+      end
+
+      if relation.key?('object')
+        puts 'object: ' + relation['object']['text']
+      end
+      puts ''
+    end
   end
 end
