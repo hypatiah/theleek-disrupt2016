fdce8995332a3161ab017ec29480e190c6681236
hypatiah <danielle.hassid@gmail.com>
2016-09-10 21:10:49 -0700

    Modify controller to grab all object, subject and actions from articles and store in resp arrays

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,26 +3,34 @@ require 'alchemyapi'
 class ArticlesController < ApplicationController
   def index
     articles = HTTParty.get('https://public-api.wordpress.com/wp/v2/sites/techcrunch.com/posts')
-    article = articles[0]['title']['rendered']
-    coder = HTMLEntities.new
-    alchemyapi = AlchemyAPI.new()
-    @article = coder.decode(article)
-    response = alchemyapi.relations("text", @article)
-    for relation in response['relations']
-      if relation.key?('subject')
-        @subject = relation['subject']['text']
-        puts 'subject: ' + relation['subject']['text']
+    @subjects = []
+    @actions = []
+    @objects = []
+    first_title = articles[0]['title']['rendered']
+    @format_first_title = HTMLEntities.new.decode(first_title)
+    articles.each do |article|
+      title = article['title']['rendered']
+      coder = HTMLEntities.new
+      alchemyapi = AlchemyAPI.new()
+      format_title = coder.decode(title)
+      response = alchemyapi.relations("text", format_title)
+
+      for relation in response['relations']
+        if relation.key?('subject')
+          @subjects << relation['subject']['text']
+        end
+
+        if relation.key?('action')
+          @actions << relation['action']['text']
+          # puts 'action: ' + relation['action']['text']
+        end
+
+        if relation.key?('object')
+          @objects << relation['object']['text']
+          # puts 'object: ' + relation['object']['text']
+        end
       end
 
-      if relation.key?('action')
-        @action = relation['action']['text']
-        puts 'action: ' + relation['action']['text']
-      end
-
-      if relation.key?('object')
-        @object = relation['object']['text']
-        puts 'object: ' + relation['object']['text']
-      end
     end
 
   end
