ede36b1ae0b878e13086d69deaeb5680cf491a6e
hypatiah <danielle.hassid@gmail.com>
2016-09-10 21:10:18 -0700

    Create basic skeleton for grabbing object, action and subject of article title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,24 +2,28 @@ require 'alchemyapi'
 
 class ArticlesController < ApplicationController
   def index
-    @articles = HTTParty.get('https://public-api.wordpress.com/wp/v2/sites/techcrunch.com/posts')
-    @article = @articles[0]['title']['rendered']
+    articles = HTTParty.get('https://public-api.wordpress.com/wp/v2/sites/techcrunch.com/posts')
+    article = articles[0]['title']['rendered']
+    coder = HTMLEntities.new
     alchemyapi = AlchemyAPI.new()
-    myText = @article
-    response = alchemyapi.relations("text", myText)
+    @article = coder.decode(article)
+    response = alchemyapi.relations("text", @article)
     for relation in response['relations']
       if relation.key?('subject')
+        @subject = relation['subject']['text']
         puts 'subject: ' + relation['subject']['text']
       end
 
       if relation.key?('action')
+        @action = relation['action']['text']
         puts 'action: ' + relation['action']['text']
       end
 
       if relation.key?('object')
+        @object = relation['object']['text']
         puts 'object: ' + relation['object']['text']
       end
-      puts ''
     end
+
   end
 end
