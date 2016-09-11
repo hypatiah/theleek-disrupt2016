1d6582dc269fe780386d5368198cbf145252d180
hypatiah <danielle.hassid@gmail.com>
2016-09-10 21:34:14 -0700

    Store relations of first article in key_terms and render

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,32 +6,60 @@ class ArticlesController < ApplicationController
     @subjects = []
     @actions = []
     @objects = []
+    @key_terms = {}
     first_title = articles[0]['title']['rendered']
     @format_first_title = HTMLEntities.new.decode(first_title)
-    articles.each do |article|
-      title = article['title']['rendered']
-      coder = HTMLEntities.new
-      alchemyapi = AlchemyAPI.new()
-      format_title = coder.decode(title)
-      response = alchemyapi.relations("text", format_title)
 
-      for relation in response['relations']
-        if relation.key?('subject')
-          @subjects << relation['subject']['text']
-        end
+    articles.each_with_index do |article, index|
+        title = article['title']['rendered']
+        coder = HTMLEntities.new
+        alchemyapi = AlchemyAPI.new()
+        format_title = coder.decode(title)
+        response = alchemyapi.relations("text", format_title)
 
-        if relation.key?('action')
-          @actions << relation['action']['text']
-          # puts 'action: ' + relation['action']['text']
-        end
+        for relation in response['relations']
+            if relation.key?('subject')
+              subject = relation['subject']['text']
+              @key_terms["subject"] = subject if index == 0
+              @subjects << subject
+            end
 
-        if relation.key?('object')
-          @objects << relation['object']['text']
-          # puts 'object: ' + relation['object']['text']
-        end
+            if relation.key?('action')
+              action = relation['action']['text']
+              @key_terms["action"] = action if index == 0
+              @actions << action
+              # puts 'action: ' + relation['action']['text']
+            end
+
+            if relation.key?('object')
+              object = relation['object']['text']
+              @key_terms["object"] = object if index == 0
+              @objects << object
+              # puts 'object: ' + relation['object']['text']
+            end  
       end
-
     end
-
   end
 end
+    # articles.each do |article|
+    #   title = article['title']['rendered']
+    #   coder = HTMLEntities.new
+    #   alchemyapi = AlchemyAPI.new()
+    #   format_title = coder.decode(title)
+    #   response = alchemyapi.relations("text", format_title)
+    #
+    #   for relation in response['relations']
+    #     if relation.key?('subject')
+    #       @subjects << relation['subject']['text']
+    #     end
+    #
+    #     if relation.key?('action')
+    #       @actions << relation['action']['text']
+    #       # puts 'action: ' + relation['action']['text']
+    #     end
+    #
+    #     if relation.key?('object')
+    #       @objects << relation['object']['text']
+    #       # puts 'object: ' + relation['object']['text']
+    #     end
+    #   end
